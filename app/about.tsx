bc2884665f7861c3d99cef0198ac869e7a1d3685
Ofallbrains <ofallbrains@gmail.com>
2025-03-22 23:55:23 +0200

    Navigating through screens

---
+++ app/about.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native'
+
+export default function AboutScreen() {
+    return (
+        <View style={{ flex: 1, backgroundColor: '#25292e', justifyContent: 'center', alignItems: 'center' }}>
+            <Text style={{ color: '#fff' }}>About Screen</Text>
+        </View>
+    )
+}
--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,12 @@
 import { Text, View, StyleSheet } from "react-native";
+import { Link } from "expo-router";
+
 
 export default function Index() {
   return (
     <View style={{ flex: 1, backgroundColor: '#25292e', alignItems: 'center', justifyContent: 'center', }}>
       <Text style={{ color: '#fff' }}>HomeScreen</Text>
+      <Link href='/about' style={{ fontSize: 20, textDecorationLine: 'underline', color: '#fff' }}>Go to About Screen</Link>
     </View>
   )
 }
