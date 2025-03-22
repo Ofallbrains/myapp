6a3f04c3e5e454e28315362d6abd9b5fc1749194
Ofallbrains <ofallbrains@gmail.com>
2025-03-22 23:33:13 +0200

    React native

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View, StyleSheet } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={{ flex: 1, backgroundColor: '#25292e', alignItems: 'center', justifyContent: 'center', }}>
+      <Text style={{ color: '#fff' }}>HomeScreen</Text>
+    </View>
+  )
+}
+
+
